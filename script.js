05a770450c36c3f3d75ab83f209c6bb3f04ae54f
Qwetuo <chewminzhuang@hotmail.com>
2018-06-10 20:21:27 +0800

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function smaller() {
+//     document.body.className='smaller';
+// }
+
+// function normal() {
+//     document.body.className='';
+// }
+
+// function bigger() {
+//     document.body.className='bigger';
+// }
+
+function changeTextSize(className) {
+    document.body.className = className;
+}
+
+function changeColor(color) {
+    document.getElementsByTagName('div').className = color;
+}
